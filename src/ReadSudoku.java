1f04afa9d63adbacf3044eff592f11778e33fd51
Robin Hellgren <rhellgren@icloud.com>
2015-03-23 14:47:03 +0100

    added basic example

--- src/ReadSudoku.java
+++ src/ReadSudoku.java
@@ -1,5 +1,48 @@
+import java.io.*;
+import java.util.ArrayList;
+
 /**
  * Created by rhellgren on 10/03/15.
  */
 public class ReadSudoku {
+    BufferedReader reader;
+
+    ReadSudoku(String filepath) throws IOException {
+        reader = new BufferedReader(new FileReader(filepath));
+    }
+
+    public ArrayList<Integer>[][] getSudokuGrid() throws IOException {
+        ArrayList<Integer>[][] sudoku = new ArrayList[9][9];
+        String line;
+        int currentRow = 0;
+        int currentColumn;
+        ArrayList<Integer> current;
+        while ((line = reader.readLine()) != null){
+            currentColumn = 0;
+            for(int i = 0; i < line.length(); i++){
+                switch (line.charAt(i)) {
+                    case ' ':
+                        current = new ArrayList<Integer>();
+                        sudoku[currentRow][currentColumn] = current;
+                        currentColumn++;
+                        break;
+                    case '|':
+                        break;
+                    case '-':
+                        break;
+                    case '+':
+                        break;
+                    default:
+                        current = new ArrayList<Integer>();
+                        current.add(Character.getNumericValue(line.charAt(i)));
+                        sudoku[currentRow][currentColumn] = current;
+                        currentColumn++;
+                        break;
+                }
+            }
+            if(currentColumn > 0)
+                currentRow++;
+        }
+        return sudoku;
+    }
 }
--- src/SolveSudoku.java
+++ src/SolveSudoku.java
@@ -1,5 +1,22 @@
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 /**
  * Created by rhellgren on 10/03/15.
  */
 public class SolveSudoku {
+    public static void main(String[] args) throws IOException {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Please enter file path: ");
+        String filePath = in.nextLine();
+
+        ReadSudoku reader = new ReadSudoku(filePath);
+        ArrayList<Integer>[][] sudoku = reader.getSudokuGrid();
+
+        System.out.println("Läst in sudoku");
+        System.out.println("5: " + sudoku[7][4].get(0));
+        System.out.println("3: " + sudoku[0][2]);
+        System.out.println("tom: " + sudoku[0][5]);
+    }
 }
