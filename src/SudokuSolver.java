8f8c38dc97c6df795c0931061ecef2aca7f52343
Axel Samuelsson <axelsam@kth.se>
2015-04-10 19:06:15 +0200

    searches in rows and columns

--- src/SudokuSolver.java
+++ src/SudokuSolver.java
@@ -7,6 +7,7 @@ public class SudokuSolver {
     SudokuSolver() {}
 
     public SudokuGrid solve(SudokuGrid sudokuGrid) {
+        float score = 0;
         boolean finished = false;
 
         int safety = 0;
@@ -20,15 +21,20 @@ public class SudokuSolver {
             switch(method) {
                 case 1: singles = searchInSquares(sudokuGrid);
                         break;
+                case 2: singles = searchInRows(sudokuGrid);
+                        break;
+                case 3: singles = searchInColumns(sudokuGrid);
+                        break;
                 default:singles = findSingles(sudokuGrid);
                         break;
             }
 
             //System.out.println("Singles: "+singles.size()+" Finals: "+sudokuGrid.numFinals());
             if(singles.size() == 0) {
-                System.out.println("Switching up strategies");
-                if(method == 0) {
-                    method = 1;
+                score += 1;
+                //System.out.println("Switching up strategies");
+                if(method >= 0 && method <= 2) {
+                    method++;
                 } else {
                     method = 0;
                 }
@@ -54,14 +60,14 @@ public class SudokuSolver {
                 System.out.println("Något är knas");
             }
         }
-
+        System.out.println("Score: "+score);
         return sudokuGrid;
     }
 
     private ArrayList searchInSquares(SudokuGrid sudokuGrid) {
         ArrayList<int[]> singles = new ArrayList<int[]>();
 
-        for(int i=0; i <=8; i++) {
+        for(int i=0; i <= 8; i++) {
             singles.addAll(searchInSquare(sudokuGrid, i));
         }
 
@@ -124,7 +130,7 @@ public class SudokuSolver {
         }
 
         //If a number is only possible in one cell, then place it as final in that cell.
-        for (int number = 1; number < 10; number++) {
+        for (int number = 1; number <= 9; number++) {
             if(numPossibles[number].size() == 1) {
                 int[] location = (int[]) numPossibles[number].get(0);
 
@@ -139,6 +145,100 @@ public class SudokuSolver {
         }
 
         return singles;
+    }
+
+    private ArrayList searchInRows(SudokuGrid sudokuGrid) {
+        ArrayList<int[]> singles = new ArrayList<int[]>();
+
+        for(int i=0; i <= 8; i++) {
+            singles.addAll(searchInRow(sudokuGrid, i));
+        }
+
+        return singles;
+    }
+
+    private ArrayList searchInRow(SudokuGrid sudokuGrid, int row) {
+        ArrayList<int[]> singles = new ArrayList<int[]>();
+
+        ArrayList[] numPossibles = new ArrayList[10];
+        for(int i=0; i <= 9; i++) {
+            numPossibles[i] = new ArrayList<int[]>();
+        }
+
+        //Every time a number is possible in a cell in the row, add that cell's location to numPossibles[number]
+        for (int column = 0; column <= 8; column++) {
+            ArrayList possibilities = sudokuGrid.getValues(row, column);
+            for(int i=0; i < possibilities.size(); i++) {
+                int[] location = new int[2];
+                location[0] = row;
+                location[1] = column;
+                numPossibles[(int) possibilities.get(i)].add(location);
+            }
+        }
+
+        //If a number is only possible in one cell, then place it as final in that cell.
+        for (int number = 1; number <= 9; number++) {
+            if(numPossibles[number].size() == 1) {
+                int[] location = (int[]) numPossibles[number].get(0);
+
+                if(!sudokuGrid.isFinal(location[0], location[1])) {
+                    int[] single = new int[3];
+                    single[0] = location[0];
+                    single[1] = location[1];
+                    single[2] = number;
+                    singles.add(single);
+                }
+            }
+        }
+
+        return singles;       
+    }
+
+    private ArrayList searchInColumns(SudokuGrid sudokuGrid) {
+        ArrayList<int[]> singles = new ArrayList<int[]>();
+
+        for(int i=0; i <= 8; i++) {
+            singles.addAll(searchInColumn(sudokuGrid, i));
+        }
+
+        return singles;
+    }
+
+    private ArrayList searchInColumn(SudokuGrid sudokuGrid, int column) {
+        ArrayList<int[]> singles = new ArrayList<int[]>();
+
+        ArrayList[] numPossibles = new ArrayList[10];
+        for(int i=0; i <= 9; i++) {
+            numPossibles[i] = new ArrayList<int[]>();
+        }
+
+        //Every time a number is possible in a cell in the row, add that cell's location to numPossibles[number]
+        for (int row = 0; row <= 8; row++) {
+            ArrayList possibilities = sudokuGrid.getValues(row, column);
+            for(int i=0; i < possibilities.size(); i++) {
+                int[] location = new int[2];
+                location[0] = row;
+                location[1] = column;
+                numPossibles[(int) possibilities.get(i)].add(location);
+            }
+        }
+
+        //If a number is only possible in one cell, then place it as final in that cell.
+        for (int number = 1; number <= 9; number++) {
+            if(numPossibles[number].size() == 1) {
+                int[] location = (int[]) numPossibles[number].get(0);
+
+                if(!sudokuGrid.isFinal(location[0], location[1])) {
+                    int[] single = new int[3];
+                    single[0] = location[0];
+                    single[1] = location[1];
+                    single[2] = number;
+                    singles.add(single);
+                }
+            }
+        }
+
+        return singles;       
     }
 
     private ArrayList findSingles(SudokuGrid sudokuGrid) {
@@ -170,7 +270,6 @@ public class SudokuSolver {
 
     private ArrayList checkSquares(SudokuGrid sudokuGrid) {
         ArrayList<int[]> singledOut = new ArrayList<int[]>();
-
 
         return singledOut;
     }
